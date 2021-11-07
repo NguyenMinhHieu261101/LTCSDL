bc3296195e264e62a7360d078a6ba10198749e8f
Nguyễn Minh Hiếu <nmh26112001@gmail.com>
2021-11-07 09:11:30 +0700

    Lab06

---
+++ Lab6_Basic_Command/Lab6_Basic_Command/FoodForm.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace Lab6_Basic_Command
+{
+    public partial class frmFood : Form
+    {
+        public frmFood()
+        {
+            InitializeComponent();
+        }
+
+        public void LoadFood(int categoryID)
+        {
+            string connectionString = "server=.; database = RestaurantManagement; Integrated Security = true;";
+            SqlConnection sqlConnection = new SqlConnection(connectionString);
+            SqlCommand sqlCommand = sqlConnection.CreateCommand();
+            sqlCommand.CommandText = "SELECT Name FROM Category where ID = " + categoryID;
+            sqlConnection.Open();
+
+            string catName = sqlCommand.ExecuteScalar().ToString();
+            this.Text = "Danh sách các món ăn thuộc nhóm : " + catName;
+            sqlCommand.CommandText = "SELECT * FROM food WHERE FoodCategoryID = " + categoryID;
+
+            SqlDataAdapter da = new SqlDataAdapter(sqlCommand);
+            DataTable dt = new DataTable("Food");
+            da.Fill(dt);
+            dgvFood.DataSource = dt;
+            sqlConnection.Close();
+            sqlConnection.Dispose();
+            da.Dispose();
+        }
+
+        private void dgvFood_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
